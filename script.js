cf88c539384a4f45a93322a9bf65fb9a20dd6530
kauakaua <kauaornelas88@gmail.com>
2023-05-21 15:04:52 -0300

    prog.maiornum

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
 console.log('Hello!');
-saudacao()
+//saudacao()
+maiorNumero()
 
  function saudacao() {
 let nome = prompt ("qual seu nome?")
+
 if (nome !="") {
     window.alert ("seja bem vindo " + nome + " ! ! ")
+}else{
+    window.alert ("poxa, queria saber seu nome :-(")
 }
 
  }
+
+ function maiorNumero() {
+    //entrada valor
+    let n1 = prompt ("qual o 1°numero?")
+    let n2 = prompt ("qual o 2°numero?")
+    let n3 = prompt ("qual o 3°numero?")
+    let maiorNumero=0 
+
+    //processamento do programa (exibir maior numero)
+    if (n1>maiorNumero) {
+    maiorNumero=n1
+    }
+
+    if (n2>maiorNumero) {
+        maiorNumero=n2
+        }
+
+        if (n3>maiorNumero) {
+        maiorNumero=n3
+    }
+
+    //saida
+window.alert ("O maior numero é:" + maiorNumero)
+
+ }
+
+ 
